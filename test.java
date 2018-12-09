88b5ab0f23fec9181b7d5c5d14d9383a590e156f
SankrGithub <45074975+sankrgithub@users.noreply.github.com>
2018-12-09 23:07:21 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ class test {
    public static void main(String[] a) {
 
       System.out.println("You have got a message from Sankr throuh Java. Hello World. !!!");
-      System.out.System.out.println("This is added in Atom.");
+      System.out.println("This is added in Atom.");
+      System.out.println("This is added in GitHub");
    }
 }
