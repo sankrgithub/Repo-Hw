261270bc65b51ea884a3fea6b85dd75668d6a5dc
SankrGithub <45074975+sankrgithub@users.noreply.github.com>
2018-12-09 22:45:01 +0530

    This is to commit changes made in Atom.

---
+++ test.java
@@ -0,0 +1,8 @@
+class test {
+
+   public static void main(String[] a) {
+
+      System.out.println("You have got a message from Sankr throuh Java. Hello World. !!!");
+      System.out.System.out.println("This is added in Atom.");
+   }
+}
